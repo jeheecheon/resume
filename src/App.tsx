194146a692fa807cc0f046805c28e9533fea4792
jeheecheon <jeheecheon@gmail.com>
2024-08-20 01:42:37 +0900

    chore: 설치 및 세팅: react, eslint, prettier, editorconfig, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-red-500">Hello, React 18!</h1>
+            <p>Check the console for logs.</p>
+        </>
+    );
+}
+
+export default App;
