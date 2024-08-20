a11021a47e8fc7936633df7d3685d22e05c8ba03
jeheecheon <jeheecheon@gmail.com>
2024-08-21 01:11:23 +0900

    chore: Projects 섹션 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Tag from './_components/Tag';
+import Tag from '@/_components/Tag';
 
 function Page() {
     return (
@@ -7,8 +7,9 @@ function Page() {
             <Page.Introduction />
             <Page.Contact />
             <Page.Projects />
+            <Page.Education />
 
-            <div className="h-[100vh]"></div>
+            <div className="h-[30vh]"></div>
         </div>
     );
 }
@@ -25,7 +26,7 @@ Page.Greeting = function Greeting() {
 
 Page.Introduction = function Introduction() {
     return (
-        <p className="text-[1.7rem] leading-[3.1rem] text-pretty">
+        <p className="text-[1.3rem] sm:text-[1.7rem] leading-[2.2rem] sm:leading-[3.1rem] text-pretty">
             {`"코드의 깊이"`}를 중시하는 프론트엔드 개발자입니다. 단순한
             기능구현을 넘어서 동작 원리를 이해하고, 최적화된 코드를 작성하는
             것을 지향합니다. 개발자의 가치는 능동적인 협업력과 성장력이라
@@ -40,14 +41,14 @@ Page.Contact = function Contact() {
         <div>
             <h2>Contact</h2>
 
-            <div className="flex gap-x-4 items-center">
-                <div className="w-[13rem] h-[13rem] bg-gray-200 rounded-3xl" />
+            <div className="flex flex-col sm:flex-row gap-x-4 items-center">
+                <div className="w-full aspect-square max-w-[14rem] bg-gray-200 rounded-full sm:rounded-3xl" />
 
-                <div className="h-[13rem] flex flex-col justify-around">
+                <div className="w-full h-[10rem] sm:w-fit sm:h-[13rem] px-3 sm:px-0 flex flex-col justify-around">
                     <div className="space-y-1">
                         <h3>Reach me</h3>
 
-                        <div>
+                        <div className="text-sm text-nowrap">
                             <p className="inline font-medium text-theme-1">
                                 {'Phone. '}
                             </p>
@@ -56,7 +57,7 @@ Page.Contact = function Contact() {
                             </a>
                         </div>
 
-                        <div>
+                        <div className="text-sm text-nowrap">
                             <p className="inline font-medium text-theme-1">
                                 {'Email. '}
                             </p>
@@ -69,7 +70,7 @@ Page.Contact = function Contact() {
                     <div className="space-y-1">
                         <h3>Channel</h3>
 
-                        <div>
+                        <div className="text-sm text-nowrap">
                             <p className="inline font-medium text-theme-1">
                                 {'Blog. '}
                             </p>
@@ -78,7 +79,7 @@ Page.Contact = function Contact() {
                             </a>
                         </div>
 
-                        <div>
+                        <div className="text-sm text-nowrap">
                             <p className="inline font-medium text-theme-1">
                                 {'Github. '}
                             </p>
@@ -106,7 +107,7 @@ Page.Projects = function Projects() {
                     명지대학교 캡스톤디자인 경진 대회
                     <span className="text-theme-1">{` (동상)`}</span>
                 </h3>
-                <p className="text-sm">
+                <p className="mt-1 text-xs sm:text-sm">
                     2024. 03. ~ 2024. 05.{'\n'}
                     팀장 | 총 4명
                 </p>
@@ -114,11 +115,12 @@ Page.Projects = function Projects() {
                 <div>
                     <h4 className="mt-5 text-theme-1">Description</h4>
 
-                    <p>
+                    <p className="text-base">
                         먹짐마 (웹앱).{'\n'}
-                        헬스장을 의미하는 Gym과 많이 먹는 것을 자제하라는
-                        의미에서 지어진 프로젝트 이름입니다. 사용자의 식단 관리
-                        및 운동 계획을 지원하는 웹앱입니다.{'\n\n'}
+                        헬스장을 의미하는 단어 Gym과 많이 먹는 것을 자제하라는
+                        의미가 포함된 프로젝트명입니다. 사용자의 식단 관리 및
+                        운동 계획을 지원하며 인스타 형식의 커뮤니티를
+                        제공합니다.{'\n\n'}
                     </p>
                     <ul>
                         <li>
@@ -158,22 +160,62 @@ Page.Projects = function Projects() {
                         </li>
                         <li>
                             <p>
-                                AWS의 EC2 인스턴스 관리 및 Nginx 세팅. Github
-                                pages를 통한 CI/CD 환경 구축
+                                AWS의 EC2 관리 및 Nginx를 세팅. Github Actions와
+                                Docker를 통한 CI/CD 환경 구축
                             </p>
                         </li>
                         <li>
                             <p>단독으로 프론트 개발</p>
                         </li>
                         <li>
-                            <p>모바일 UI/UX 디자인</p>
+                            <p>모바일 친화적인 UI/UX 디자인</p>
                         </li>
                     </ul>
                 </div>
 
                 <div>
                     <h4 className="mt-5 text-theme-1">Project Review</h4>
-                    <p></p>
+                    <p>
+                        프로젝트 특성에 맞춰 모바일 친화적인 UI/UX를 디자인
+                        했습니다. Figma와 같은 디자인 툴을 다뤄본 적이 없기에
+                        간단히 스케치를 곧바로 코드로 옮기는 방법을 택했는데
+                        세심한 디테일에 신경쓰다보니 기존 디자인을 수정하는 일이
+                        잦아 많은 시간이 할애됐습니다.{'\n'}
+                        비동기 AI Call이 필요한 경우 유저에게 실시간 로딩 상태를
+                        표시하여 반응성 높은 UX를 제공했습니다. Client-Side 에서
+                        비동기 로직을 다룰 때는 외부 상태관리 라이브러리 도움
+                        없이 React의 Suspense API를 활용하여 직접 처리해보기도
+                        했습니다.{'\n'}
+                        원할한 협업을 위해 Git 컨벤션을 정의했고 Squash-Merge
+                        등을 활용하여 PR 관리를 하려 했으나 백엔드의 배포 상태에
+                        결함이 있어 후반부에 제대로 지켜지지 못했습니다.
+                    </p>
+                </div>
+
+                <div className="mt-5 text-theme-1">
+                    <h4>Links</h4>
+                    <p className="text-sm">
+                        <span>시연 영상. </span>
+                        <a
+                            title="먹짐마 시연 영상"
+                            href="https://www.youtube.com/watch?v=lX7CzpYh6HU"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://www.youtube.com/watch?v=lX7CzpYh6HU
+                        </a>
+                    </p>
+                    <p className="text-sm">
+                        <span>깃허브 리포. </span>
+                        <a
+                            title="먹짐마 깃허브 리포"
+                            href="https://github.com/jeheecheon/muckGYMma"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://github.com/jeheecheon/muckGYMma
+                        </a>
+                    </p>
                 </div>
 
                 <div>
@@ -185,16 +227,16 @@ Page.Projects = function Projects() {
                         <Tag>ReduxTK</Tag>
                         <Tag>AWS</Tag>
                         <Tag>Docker</Tag>
-                        <Tag>github pages</Tag>
+                        <Tag>github actions</Tag>
                     </div>
                 </div>
             </div>
 
             <div className="mt-[4rem]">
                 <h3>개인 블로그 개발</h3>
-                <p className="text-sm">
+                <p className="mt-1 text-xs sm:text-sm">
                     2024. 01. ~ 2024. 02.{'\n'}
-                    개인 프로젝트
+                    개인 프로젝트 (유지보수 중)
                 </p>
 
                 <div>
@@ -213,8 +255,8 @@ Page.Projects = function Projects() {
                         </li>
                         <li>
                             <p>
-                                AWS의 EC2 인스턴스 관리 및 Nginx 세팅. Github
-                                pages를 통한 CI/CD 환경 구축
+                                AWS의 EC2 관리 및 Nginx를 세팅. Github actions와
+                                Docker를 통한 CI/CD 환경 구축
                             </p>
                         </li>
                         <li>
@@ -223,7 +265,19 @@ Page.Projects = function Projects() {
                         <li>
                             <p>
                                 AWS S3와 개인 DB를 활용하여 블로그 글에 첨부되는
-                                이미지 저장
+                                이미지 관리
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Quill html 편집 모듈을 통해 실시간으로 글 업로드
+                                및 수정
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                CSRF 공격을 막기 위해 DB에 저장하기 전
+                                백엔드에서 문자열을 정제
                             </p>
                         </li>
                     </ul>
@@ -231,7 +285,40 @@ Page.Projects = function Projects() {
 
                 <div>
                     <h4 className="mt-5 text-theme-1">Project Review</h4>
-                    <p></p>
+                    <p>
+                        다소 느린 배포 서버가 사용자 경험에 미치는 영향을
+                        최소화하기 위해 스켈레톤 UI 컴포넌트를 만들었습니다.
+                        비동기 상태관리는 react-query를 사용하여 간단하게
+                        처리했습니다. CSRF 공격을 막기 위해 백엔드에서 문자열을
+                        정제하는 방법을 사용했습니다. 백엔드와 프론트의 전체적인
+                        흐름을 경험하고 배포까지 해보았습니다.
+                    </p>
+                </div>
+
+                <div className="mt-5 text-theme-1">
+                    <h4>Links</h4>
+                    <p className="text-sm">
+                        <span>배포 링크. </span>
+                        <a
+                            title="개인 블로그"
+                            href="https://blog.jeheecheon.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://blog.jeheecheon.com
+                        </a>
+                    </p>
+                    <p className="text-sm">
+                        <span>깃허브 리포. </span>
+                        <a
+                            title="개인 블로그 깃허브 리포"
+                            href="https://github.com/jeheecheon/blog"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://github.com/jeheecheon/blog
+                        </a>
+                    </p>
                 </div>
 
                 <div>
@@ -248,9 +335,134 @@ Page.Projects = function Projects() {
                         <Tag>AWS</Tag>
                         <Tag>S3</Tag>
                         <Tag>Docker</Tag>
-                        <Tag>github pages</Tag>
+                        <Tag>github actions</Tag>
                     </div>
                 </div>
+            </div>
+
+            <div className="mt-[4rem]">
+                <h3>AO 스터디 공식 웹사이트</h3>
+                <p className="mt-1 text-xs sm:text-sm">
+                    2024. 01. ~ 2024. 02.{'\n'}
+                    Team 1 멘토 | 총 12명
+                </p>
+
+                <div>
+                    <h4 className="mt-5 text-theme-1">What I did</h4>
+                    <ul>
+                        <li>
+                            <p>
+                                Team 1의 멘토(팀장)으로 참여하여 프론트팀과
+                                백엔드팀의 질문 답변 및 코드 리뷰
+                            </p>
+                        </li>
+                        <li>
+                            <p>Team 1의 git 컨벤션 구성</p>
+                        </li>
+                        <li>
+                            <p>
+                                AWS의 EC2 관리 및 Nginx를 세팅. Github actions와
+                                Docker를 통한 CI/CD 환경 구축
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Team 1과 Team 2의 프론트엔드 코드베이스 통합.
+                                Svelte 코드를 React 코드로 수정
+                            </p>
+                        </li>
+                        <li>
+                            <p>다크 / 라이트 모드 구현</p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4 className="mt-5 text-theme-1">Project Review</h4>
+                    <p>
+                        프론트와 백엔드의 코드베이스 통합 작업을 주도했습니다.
+                        특히 Svelte로 작성된 코드베이스를 React로 변환하며,
+                        프론트엔드 팀의 개발을 가이드하고 코드 리뷰를
+                        진행했습니다. 프로젝트 전반에 걸쳐 팀원들과 협업하며
+                        문제를 해결하고, 효율적인 개발 환경을 조성하는 데
+                        기여했습니다.
+                    </p>
+                </div>
+
+                <div className="mt-5 text-theme-1">
+                    <h4>Links</h4>
+                    <p className="text-sm">
+                        <span>배포 링크. </span>
+                        <a
+                            title="AO 스터디 공식 웹사이트"
+                            href="https://www.fireao.com/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://www.fireao.com/
+                        </a>
+                    </p>
+                    <p className="text-sm">
+                        <span>깃허브 리포. </span>
+                        <a
+                            title="AO 스터디 공식 웹사이트 깃허브 리포"
+                            href="https://github.com/Fire-AO/official-website"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://github.com/Fire-AO/official-website
+                        </a>
+                    </p>
+                </div>
+
+                <div>
+                    <h4 className="mt-5 text-theme-1">Skillset I Used</h4>
+                    <div className="flex flex-wrap mt-1 gap-2">
+                        <Tag>react.js</Tag>
+                        <Tag>tailwindcss</Tag>
+                        <Tag>typescript</Tag>
+                        <Tag>AWS</Tag>
+                        <Tag>Docker</Tag>
+                        <Tag>github actions</Tag>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-[4rem]">
+                <h3>Etc</h3>
+                <p className='mt-1'>
+                    이외 진행했던 프로젝트들은 아래 링크에서 확인할 수 있습니다.
+                </p>
+                <p className="text-xs sm:text-sm">
+                    <span className='font-medium text-theme-1'>Projects Cagetory. </span>
+                    <a
+                        title="Projects 카테고리"
+                        href="https://blog.jeheecheon.com/categories/Projects"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        https://blog.jeheecheon.com/categories/Projects
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+Page.Education = function Education() {
+    return (
+        <div>
+            <h2>Education</h2>
+            <div className="mt-[1.5rem]">
+                <h3 className="text-theme-1">명지대학교 - 컴퓨터공학과</h3>
+                <p className="text-xs sm:text-sm">
+                    2018. 03. ~ 2025. 02. (재학 중)
+                </p>
+            </div>
+
+            <div className="mt-[1.5rem]">
+                <h3 className="text-theme-1">토익 - 855점</h3>
+                <p className="text-xs sm:text-sm">2023. 08. 06.</p>
             </div>
         </div>
     );
