13ef93599d38709e8504d4459224eb4822395e59
jeheecheon <jeheecheon@gmail.com>
2024-08-22 22:14:33 +0900

    chore: 이력서 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -478,23 +478,36 @@ Page.AwardsAndScores = function AwardsAndScores() {
             <h2>Awards & Scores</h2>
             <div>
                 <h3 className="text-theme-1">
-                    캡스톤디자인 경진 대회 -
+                    <span>캡스톤디자인 경진 대회 -</span>
                     <span className="dark:text-inverse-1">{` 동상`}</span>
                 </h3>
-                <p className="text-xs sm:text-sm">2024. 06. 10.</p>
+                <p className="text-xs sm:text-sm">
+                    <span>2024. 06. 10.{`\n`}</span>
+                    <span>
+                        캡스톤디자인 경진대회는 실생활 문제를 해결하는 창의적인
+                        소프트웨어 또는 하드웨어 프로젝트를 평가하는 대회입니다.
+                    </span>
+                </p>
             </div>
 
             <div className="mt-[1.5rem]">
                 <h3 className="text-theme-1">
-                    디지털 리터리스 PT 경진대회 -
+                    <span>디지털 리터리스 PT 경진대회 -</span>
                     <span className="dark:text-inverse-1">{` 우수상`}</span>
                 </h3>
-                <p className="text-xs sm:text-sm">2023. 12. 27.</p>
+                <p className="text-xs sm:text-sm">
+                    <span>2023. 12. 27.{`\n`}</span>
+                    <span>
+                        디지털 리터러시 PT 경진대회는 디지털 도구와 정보 활용
+                        능력을 평가하는 발표 대회입니다.
+                    </span>
+                </p>
             </div>
 
             <div className="mt-[1.5rem]">
                 <h3 className="text-theme-1">
-                    토익 -<span className="dark:text-inverse-1">{` 855점`}</span>
+                    <span>토익 -</span>
+                    <span className="dark:text-inverse-1">{` 855점`}</span>
                 </h3>
                 <p className="text-xs sm:text-sm">2023. 08. 06.</p>
             </div>
@@ -508,7 +521,7 @@ Page.Education = function Education() {
             <h2>Education</h2>
             <div className="mt-[1.5rem]">
                 <h3 className="text-theme-1">
-                    명지대학교 -
+                    <span>명지대학교 -</span>
                     <span className="dark:text-inverse-1">{` 우수상`}</span>
                 </h3>
                 <p className="text-xs sm:text-sm">
